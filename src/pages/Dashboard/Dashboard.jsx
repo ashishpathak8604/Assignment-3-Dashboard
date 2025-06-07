5cf599dd05255cea91d9c50f4f64063e3e172a0f
ashishpathak8604 <ashishpathakfighter@gmial.com>
2025-06-07 14:55:20 +0530

    /

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import OverviewCards from './OverviewCards';
 import UserActivityChart from './UserActivityChart';
 import TaskDistributionChart from './TaskDistributionChart';
 import RecentActivityTable from './RecentActivityTable';
 
 const Dashboard = () => {
+    useEffect(() => {
+    const alertShown = localStorage.getItem('dashboardAlertShown');
+    if (!alertShown) {
+      alert('This page is under work. It is not my full work and will be completed by week 4.');
+      localStorage.setItem('dashboardAlertShown', 'true');
+    }
+  }, []);
+
   return (
     <div className="space-y-6">
       <OverviewCards />
